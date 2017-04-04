0413933cd6fe1fd19004005324449f26ef55a50c
Thomas A Caswell <tcaswell@gmail.com>
2017-04-04 18:15:40 -0400

    BLD: correctly pin hdf5 version on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 "%PYTHON%" setup.py configure --hdf5="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
 
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
+"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt --hdf5-version=1.8.17
 if errorlevel 1 exit 1
