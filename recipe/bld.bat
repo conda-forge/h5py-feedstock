b953d6ea0df359959ea7ceb66b7f5372135601a9
Thomas A Caswell <tcaswell@gmail.com>
2017-04-04 19:00:34 -0400

    BLD: put the version in the right place

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-"%PYTHON%" setup.py configure --hdf5="%LIBRARY_PREFIX%"
+"%PYTHON%" setup.py configure --hdf5="%LIBRARY_PREFIX%"  --hdf5-version=1.8.17
 if errorlevel 1 exit 1
 
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt --hdf5-version=1.8.17
+"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
