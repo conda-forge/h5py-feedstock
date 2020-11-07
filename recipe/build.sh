6a4003ee8d0f9a36cd2773419a31b8ba67f717cb
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-11-07 00:04:06 +0000

    Revert to numpy pinning
    * relax the numpy runtime version requirements, as in the h5py pypi
      package those were picked for wheel availability. However, for us
      that is irrelevant.
    * disable the setup.py build time package installations, as for us
      this happens via the recipe.
    * set minimum mpi4py version

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,7 @@ if [[ "$mpi" != "nompi" ]]; then
   export HDF5_MPI="ON"
 fi
 
+# tell setup.py to not 'pip install' exact package requirements
+export H5PY_SETUP_REQUIRES="0"
+
 "${PYTHON}" -m pip install . --no-deps --ignore-installed --no-cache-dir -vv
