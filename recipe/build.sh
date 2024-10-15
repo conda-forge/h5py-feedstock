5851c2eb89ea64b147d4da0ea61a241a9a316ce4
Mark Harfouche <mark.harfouche@gmail.com>
2024-10-15 19:55:41 -0400

    Remove workarounds for osx arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+set -ex
 
 export HDF5_VERSION=${hdf5}
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
@@ -10,11 +10,6 @@ fi
 export OPAL_PREFIX=${PREFIX}
 if [[ "$mpi" != "nompi" ]]; then
   export HDF5_MPI="ON"
-fi
-
-if [[ ${target_platform} == "osx-arm64" ]]; then
-  # disable complex256 on macOS ARM64, see https://github.com/h5py/h5py/pull/2065
-  export CIBW_ARCHS_MACOS=arm64
 fi
 
 # tell setup.py to not 'pip install' exact package requirements
